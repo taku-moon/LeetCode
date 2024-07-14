7f0b17746fe6fd5d5edb4dcc624e5a380cac2bce
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-15 08:02:05 +0900

    Time: 16 ms (20.53%), Space: 44.6 MB (39.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // 문자열 s를 소문자로 변환한 후 영문자와 숫자를 제외한 나머지를 제거
+        // 문자열 s는 대소문자를 구분하지 않음 -> 소문자 또는 대문자로 변경
+        // 문자열 s는 영어 및 숫자로 이루어져 있음 -> 이를 제외한 나머지 제거
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
 
         // StringBuilder를 사용하여 문자열 s 뒤집기
