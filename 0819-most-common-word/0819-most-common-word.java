632553397ce35f240a186e57a319363a65ee8440
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-18 23:05:31 +0900

    Time: 11 ms (88.78%), Space: 42.8 MB (66.96%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -1,9 +1,9 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         // 문제의 조건에 맞게 전처리 작업 후 문자열을 단어 배열로 저장
-        String[] words = paragraph.toLowerCase().replaceAll("[^a-z]", " ").split("\\s+");
-        // String 배열인 금지어 목록을 비교 메서드 contains()를 제공하는 List로 바꾸기
-        List<String> ban = new ArrayList<>(Arrays.asList(banned));
+        String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
+        // String 배열인 금지어 목록을 비교 메서드 contains()를 제공하는 Set으로 바꾸기
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
 
         // 단어(Key) + 단어별 등장 횟수(Value) 저장
         Map<String, Integer> map = new HashMap<>();
@@ -15,7 +15,7 @@ class Solution {
             }
         }
 
-        // map에 저장된 데이터(entrySet) 중에서 등장 횟수(value)가 가장 큰 데이터의 단어(key) 반환 
+        // map에 저장된 데이터(entrySet) 중에서 등장 횟수(value)가 가장 많은 데이터의 단어(key) 반환
         return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
