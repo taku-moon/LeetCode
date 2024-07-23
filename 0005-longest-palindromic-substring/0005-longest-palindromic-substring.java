f273e77e5e33b9613092af6ffdc29ca2f1cab19c
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-24 01:53:19 +0900

    Time: 15 ms (88.01%), Space: 42.4 MB (71.57%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,29 +4,33 @@ class Solution {
     int palindromeLen;
 
     public String longestPalindrome(String s) {
+        // 오른쪽으로 한 칸씩 이동하며 투 포인터 조사
         for (int i = 0; i < s.length() - 1; i++) {
-            extendPalindrome(s, i, i + 1);
-            extendPalindrome(s, i, i + 2);
+            extendPalindrome(s, i, i + 1);  // 2칸짜리 투 포인터
+            extendPalindrome(s, i, i + 2);  // 3칸짜리 투 포인터
         }
 
         return s.substring(startIdx, endIdx + 1);
     }
 
     private void extendPalindrome(String s, int start, int end) {
+        // tmpXx 변수를 사용하면 start와 end 값을 추가적인 연산 없이 빠르게 파악할 수 있다.
         int tmpStart = 0;
         int tmpEnd = 0;
         int tmpLen = 0;
 
+        // 투 포인터가 유효한 범위 내에 있고, 양쪽 끝 문자가 일치하는 팰린드롬인 경우 범위 확장
         while ((start >= 0) && (end < s.length()) && (s.charAt(start) == s.charAt(end))) {
             tmpStart = start--;
             tmpEnd = end++;
             tmpLen = tmpEnd - tmpStart + 1;
         }
 
+        // 기존의 최대 길이보다 큰 경우 값 교체
         if (tmpLen > palindromeLen) {
             startIdx = tmpStart;
             endIdx = tmpEnd;
-            palindromeLen = endIdx - startIdx + 1;
+            palindromeLen = tmpLen;
         }
     }
 }
