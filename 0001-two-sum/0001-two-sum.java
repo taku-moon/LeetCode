25b7871153dfafa015b9f78321c80e6f3c1a6b62
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-25 02:18:22 +0900

    Time: 4 ms (63.12%), Space: 44.9 MB (37.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        // key(nums의 요소 값) - value(인덱스)
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            // target에서 nums[i]를 뺀 결과를 key로 조회하여 존재 여부 확인
+            // 해당 key가 존재할 경우 그 key의 value인 인덱스가 현재 인덱스 i가 아닌 경우 정답으로 리턴
+            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
             }
         }
+        
         return null;
     }
 }
