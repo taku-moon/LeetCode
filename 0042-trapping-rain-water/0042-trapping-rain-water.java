914f0079251eab4ebda3fa6faa5928c46484ee47
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-27 17:25:56 +0900

    Time: 0 ms (100%), Space: 46.4 MB (21.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,18 @@
 class Solution {
     public int trap(int[] height) {
-        int result= 0;
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
+        int water = 0;
+        int leftIdx = 0;
+        int rightIdx = height.length - 1;
+        int leftMaxHeight = height[leftIdx];
+        int rightMaxHeight = height[rightIdx];
 
-        // 투 포인터가 서로 겹칠 때까지 반복
-        while (left < right) {
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
+        while (leftIdx < rightIdx) {
+            leftMaxHeight = Math.max(leftMaxHeight, height[leftIdx]);
+            rightMaxHeight = Math.max(rightMaxHeight, height[rightIdx]);
 
-            // 더 높은 쪽을 향해 투 포인터 이동
-            if (leftMax <= rightMax) {
-                // 왼쪽 막대 최대 높이와의 차이를 더하고 왼쪽 포인터 이동
-                result += leftMax - height[left];
-                left++;
-            } else {
-                // 오른쪽 막대 최대 높이와의 차이를 더하고 오른쪽 포인터 이동
-                result += rightMax - height[right];
-                right--;
-            }
+            water += (leftMaxHeight <= rightMaxHeight) ? leftMaxHeight - height[leftIdx++] : rightMaxHeight - height[rightIdx--];
         }
 
-        return result;
+        return water;
     }
 }
