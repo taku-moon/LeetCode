a0a96bfecbf9d531a48158f2a00d640c121c2b21
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-30 03:14:23 +0900

    Time: 535 ms (23.33%), Space: 54.4 MB (5.17%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,24 +5,21 @@ class Solution {
         Set<List<Integer>> set = new LinkedHashSet<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
-            int num = nums[i];
             int leftIdx = i + 1;
             int rightIdx = nums.length - 1;
 
             while (leftIdx < rightIdx) {
-                int sum = num + nums[leftIdx] + nums[rightIdx];
+                int sum = nums[i] + nums[leftIdx] + nums[rightIdx];
 
                 if (sum == 0) {
                     List<Integer> list = new ArrayList<>();
-                    list.add(num);
+                    list.add(nums[i]);
                     list.add(nums[leftIdx]);
                     list.add(nums[rightIdx]);
                     set.add(list);
                     leftIdx++;
-
                 } else if (sum < 0) {
                     leftIdx++;
-
                 } else if (sum > 0) {
                     rightIdx--;
                 }
