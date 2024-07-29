466d1c693884973981800cb8628f0cd5b2c22ef6
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-30 04:08:27 +0900

    Time: 32 ms (49.61%), Space: 51.2 MB (74.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,47 +1,47 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 투 포인터 사용을 위해 배열을 정렬
         Arrays.sort(nums);
 
-        // 중복 값을 허용하지 않으므로 Set에 List<Integer> 담기
-        // Set에 입력한 순서를 유지하기 위해 LinkedHashSet 사용
-        Set<List<Integer>> set = new LinkedHashSet<>();
-        
-        int leftIdx;
-        int rightIdx;
-        int sum;
+        int leftIdx, rightIdx, sum;
+        List<List<Integer>> answer = new ArrayList<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
+            // 중복된 값 건너뛰기
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            // 간격을 좁혀가며 합 sum 계산
             leftIdx = i + 1;
             rightIdx = nums.length - 1;
-
-            // 간격을 좁혀가며 세 수의 합 sum 계산
             while (leftIdx < rightIdx) {
                 sum = nums[i] + nums[leftIdx] + nums[rightIdx];
-                
-                // sum이 0인 경우 정답 처리
+
+                // 합이 0인 경우 정답처리
                 if (sum == 0) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[leftIdx]);
-                    list.add(nums[rightIdx]);
-                    set.add(list);
-                    
-                    // 정답이 나왔으니 투 포인터 양쪽 모두 한 칸씩 이동
+                    answer.add(Arrays.asList(nums[i], nums[leftIdx], nums[rightIdx]));
+
+                    // 중복된 값 건너뛰기, 이 처리를 하지 않으면 같은 정답이 두 번 나올 수 있음
+                    while (leftIdx < rightIdx && nums[leftIdx] == nums[leftIdx+1]) {
+                        leftIdx++;
+                    }
+                    while (leftIdx < rightIdx && nums[rightIdx] == nums[rightIdx-1]) {
+                        rightIdx--;
+                    }
+                    // 정답이 나왔으므로 투 포인터 모두 한 칸씩 이동
+                    // 합이 0인 상황이므로 양쪽 모두 이동
                     leftIdx++;
                     rightIdx--;
-                    
+
                     // 합이 0보다 작다면 왼쪽 포인터 이동
                 } else if (sum < 0) {
                     leftIdx++;
-                    
                     // 합이 0보다 크다면 오른쪽 포인터 이동
                 } else if (sum > 0) {
                     rightIdx--;
                 }
             }
         }
-
-        return new ArrayList<>(set);
+        return answer;
     }
 }
