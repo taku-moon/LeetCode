2b7cd6b5b8800bba5b7c6c8c0b0d0124266497e5
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-30 03:01:10 +0900

    Time: 29 ms (73.73%), Space: 51.3 MB (74.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int left, right, sum;
+        List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
-
-        Set<List<Integer>> set = new LinkedHashSet<>();
-
-        for (int i = 0; i < nums.length-2; i++) {
-            int num = nums[i];
-            int firstIdx = i + 1;
-            int secondIdx = nums.length-1;
-            while (firstIdx < secondIdx) {
-                int tmp = nums[firstIdx]+nums[secondIdx];
-                if (tmp == -num) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(num);
-                    list.add(nums[firstIdx]);
-                    list.add(nums[secondIdx]);
-                    set.add(list);
-                    firstIdx++;
-                } else if (tmp > -num) {
-                    secondIdx--;
-                } else if (tmp < -num) {
-                    firstIdx++;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            left = i + 1;
+            right = nums.length - 1;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    while (left < right && nums[left] == nums[left+1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right-1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
                 }
             }
         }
-
-        return new ArrayList<>(set);
+        return answer;
     }
 }
