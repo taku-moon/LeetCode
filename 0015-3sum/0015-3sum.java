f68d5581736e2e65a9dcd2b209338f0e4ad43631
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-07-30 04:06:42 +0900

    Time: 310 ms (28.4%), Space: 53.6 MB (6.92%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // 투 포인터 사용을 위해 배열을 정렬
         Arrays.sort(nums);
 
+        // 중복 값을 허용하지 않으므로 Set에 List<Integer> 담기
+        // Set에 입력한 순서를 유지하기 위해 LinkedHashSet 사용
         Set<List<Integer>> set = new LinkedHashSet<>();
+        
+        int leftIdx;
+        int rightIdx;
+        int sum;
 
         for (int i = 0; i < nums.length - 2; i++) {
-            int leftIdx = i + 1;
-            int rightIdx = nums.length - 1;
+            leftIdx = i + 1;
+            rightIdx = nums.length - 1;
 
+            // 간격을 좁혀가며 세 수의 합 sum 계산
             while (leftIdx < rightIdx) {
-                int sum = nums[i] + nums[leftIdx] + nums[rightIdx];
-
+                sum = nums[i] + nums[leftIdx] + nums[rightIdx];
+                
+                // sum이 0인 경우 정답 처리
                 if (sum == 0) {
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
                     list.add(nums[leftIdx]);
                     list.add(nums[rightIdx]);
                     set.add(list);
+                    
+                    // 정답이 나왔으니 투 포인터 양쪽 모두 한 칸씩 이동
                     leftIdx++;
                     rightIdx--;
+                    
+                    // 합이 0보다 작다면 왼쪽 포인터 이동
                 } else if (sum < 0) {
                     leftIdx++;
+                    
+                    // 합이 0보다 크다면 오른쪽 포인터 이동
                 } else if (sum > 0) {
                     rightIdx--;
                 }
