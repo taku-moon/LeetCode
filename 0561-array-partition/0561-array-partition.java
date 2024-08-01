6e3c08c891f717a54b50e3244c8b05df4d99a324
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-08-01 23:17:50 +0900

    Time: 12 ms (96.24%), Space: 46.6 MB (28.44%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -4,9 +4,9 @@ class Solution {
 
         int sum = 0;
         for(int i = 0; i < nums.length; i += 2) {
-            sum += Math.min(nums[i], nums[i+1]);
+            sum += nums[i];
         }
-        
+
         return sum;
     }
 }
