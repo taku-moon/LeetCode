b0794108770650328bf124121d433467f01a388a
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-08-06 01:37:30 +0900

    Time: 1 ms (99.82%), Space: 61.7 MB (43.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // 최대 이익은 0으로 초기화
         int answer = 0;
+        // 저점은 임시로 첫 번째 값 지정
         int minPrice = prices[0];
 
+        // prices를 순회하면서
         for (int price : prices) {
-            if (minPrice < price) {
-                answer = Math.max(price - minPrice, answer);
-                continue;
-            }
-            if (minPrice > price) {
-                minPrice = price;
-                continue;
-            }
+            // 저점 계산
+            minPrice = Math.min(minPrice, price);
+            // 최대 이익 계산
+            answer = Math.max(answer, price - minPrice);
         }
 
         return answer;
