6a6c8ad8842893965732f57b23f31cf6df5a6d76
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-08-16 20:34:03 +0900

    Time: 0 ms (100%), Space: 40.9 MB (80.54%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -12,11 +12,14 @@ class Solution {
     public ListNode swapPairs(ListNode head) {
         ListNode answer = head;
 
+        // head와 head.next 둘 중 하나라도 존재하지 않으면 탈출
         while (head != null && head.next != null) {
+            // 임시 변수를 사용하여 값 교환
             int tmp = head.val;
             head.val = head.next.val;
             head.next.val = tmp;
 
+            // 두 칸 앞으로 이동
             head = head.next.next;
         }
 
