121d5d801c698c9f9da2d2ac4d2f44e5589dd1ce
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-08-16 21:47:16 +0900

    Time: 0 ms (100%), Space: 41.4 MB (17.37%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,20 +10,17 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode node = new ListNode(0, head);
-        ListNode answer = node;
-
-        while (node.next != null && node.next.next != null) {
-            ListNode a = node.next;
-            ListNode b = node.next.next;
-
-            node.next = b;
-            a.next = b.next;
-            b.next = a;
-
-            node = node.next.next;
+        // 현재 노드와 다음 노드가 있으면 반복
+        if (head != null && head.next != null) {
+            // 다음 노드 선언
+            ListNode p = head.next;
+            // 다음 다음 노드를 파라미터로 전달하고 스왑된 값을 리턴받음
+            head.next = swapPairs(head.next.next);
+            // 다음 다음 노드는 현재 노드로 지정
+            p.next = head;
+            // 다음 노드 리턴
+            return p;
         }
-
-        return answer.next;
+        return head;
     }
 }
