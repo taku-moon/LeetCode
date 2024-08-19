3fb3ef928440843b56c25cec662c16d36b444a97
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-08-20 03:28:30 +0900

    Time: 0 ms (100%), Space: 44.5 MB (47.15%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,23 +10,26 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        // 예외처리
         if (head == null){
             return head;
         }
 
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode rememberNode = head.next;
+        ListNode odd = head;                    // 홀수 노드 (1, 3, 5, ...)
+        ListNode even = head.next;              // 짝수 노드 (2, 4, 6, ...)
+        ListNode firstEvenNode = head.next;     // 짝수 노드의 의 첫 번째 노드 (2)
 
+        // 짝수 노드가 null이거나, 짝수 노드의 다음 노드가 null이면 while() 탈출
         while(even != null && even.next != null) {
-            odd.next = even.next;
-            odd = even.next;
+            odd.next = odd.next.next;
+            even.next = even.next.next;
 
-            even.next = odd.next;
-            even = odd.next;
+            odd = odd.next;
+            even = even.next;
         }
 
-        odd.next = rememberNode;
+        // 홀수 노드의 마지막과 짝수 노드의 첫 번째를 연결
+        odd.next = firstEvenNode;
 
         return head;
     }
