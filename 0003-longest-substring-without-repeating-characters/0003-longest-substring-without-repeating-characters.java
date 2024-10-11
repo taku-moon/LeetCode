ba68dae849a3b3a7c77bc9696c12f5eaf2286294
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-10-12 03:19:15 +0900

    Time: 5 ms (87.49%), Space: 44.9 MB (26.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,19 +1,22 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        char[] array = s.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();    // Key = 문자, Value = 인덱스
 
         int start = 0;
         int end = 0;
         int maxLen = 0;
 
-        while (end < s.length()) {
-            if (map.containsKey(array[end])) {
-                map.remove(array[start++]);
-            } else {
-                map.put(array[end], end++);
-                maxLen = Math.max(maxLen, map.size());
+        // 문자열을 문자 단위로 반복
+        for (char c : s.toCharArray()) {
+            // 이미 등장했던 문자이고, 슬라이딩 윈도우의 안쪽에 있다면 start 위치 업데이트
+            if (map.containsKey(c) && start <= map.get(c)) {
+                start = map.get(c) + 1;
+            } else {    // 최대 부분 문자열 길이 업데이트
+                maxLen = Math.max(maxLen, end - start + 1);
             }
+
+            // 문자 및 인덱스 삽입
+            map.put(c, end++);
         }
 
         return maxLen;
