f1dd5aa0c34bd89c31d670e0b0035f8701e9c40a
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-11-02 16:04:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> answer = new ArrayList<>();
-        if (digits == null || digits.length() == 0) {
+        if (digits == null || digits.length == 0) {
             return answer;
         }
 
@@ -15,22 +15,25 @@ class Solution {
         map.put('8', "tuv");
         map.put('9', "wxyz");
 
-        dfs(digits, map, 0, new StringBuilder(), answer);
+        dfs(digits, map, 0, new StringBuilder, answer);
 
-        return answer;
+        return asnwer;
     }
 
-    private void dfs(String digits, Map<Character, String> map, int index, StringBuilder sb, List<String> answer) {
-        if (index == digits.length()) {
+    public void dfs(String digits, Map<Character, String> map, int idx, StringBuilder sb, List<String> answer) {
+        if (idx == digits.length()) {
             answer.add(sb.toString());
-            return;
+            retrun;
         }
-
-        String letters = map.get(digits.charAt(index));
+        
+        String letters = map.get(digits.charAt(idx))
         for (char letter : letters.toCharArray()) {
             sb.append(letter);
-            dfs(digits, map, index + 1, sb, answer);
-            sb.deleteCharAt(sb.length() - 1); // 백트래킹
+            dfs(digits, map, idx+1, sb, answer);
+            sb.deleteCharAt(sb.length-1);
         }
+
+
+
     }
 }
