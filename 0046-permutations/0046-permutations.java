68a0687aa97c0542344e5ed174b0346250d58ad4
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-11-09 03:11:22 +0900

    Time: 2 ms (44.68%), Space: 44.8 MB (6.81%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,33 +1,33 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> numsList = new ArrayList<>();
         for (int num : nums) {
-            list.add(num);
+            numsList.add(num);
         }
         List<Integer> tmp = new ArrayList<>();
         List<List<Integer>> answer = new ArrayList<>();
 
-        dfs(list, tmp, answer);
+        dfs(numsList, tmp, answer);
 
         return answer;
     }
 
-    public void dfs(List<Integer> list, List<Integer> tmp, List<List<Integer>> answer) {
-        if (list.isEmpty()) {
+    public void dfs(List<Integer> numsList, List<Integer> tmp, List<List<Integer>> answer) {
+        if (numsList.isEmpty()) {
             answer.add(tmp);
             return;
         }
-        for (Integer num : list) {
+
+        for (Integer num : numsList) {
             tmp.add(num);
+
             List<Integer> newTmp = new ArrayList<>(tmp);
-            List<Integer> newList = new ArrayList<>(list);
+            List<Integer> newList = new ArrayList<>(numsList);
             newList.remove(num);
+            
             dfs(newList, newTmp, answer);
+
             tmp.remove(num);
         }
     }
-
-
-
-
 }
