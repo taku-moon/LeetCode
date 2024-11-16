3e9d0eef9a3774b2ad23de89f028364be1ca1648
문타쿠 <92698638+taku-moon@users.noreply.github.com>
2024-11-16 15:05:16 +0900

    Time: 17 ms (88.37%), Space: 94.5 MB (61.79%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,27 +1,21 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answer = new ArrayList<>();
+
         dfs(1, n, k, new ArrayList<>(), answer);
+
         return answer;
-
-
-
-
     }
 
     public void dfs(int start, int n, int k, List<Integer> tmp, List<List<Integer>> answer) {
         if (tmp.size() == k) {
-            answer.add(tmp);
+            answer.add(new ArrayList<>(tmp));
             return;
         }
         for (int i = start; i <= n; i++) {
             tmp.add(i);
-            List<Integer> newTmp = new ArrayList<>(tmp);
-            dfs(i+1, n, k, newTmp, answer);
+            dfs(i+1, n, k, tmp, answer);
             tmp.removeLast();
         }
-
-
-
     }
 }
